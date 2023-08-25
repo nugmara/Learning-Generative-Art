e85f6f84bcc9d67483c54532eb6fa2c1d86832c5
Dagmara Grabowska <dagmarag0@gmail.com>
2023-08-25 12:13:28 +0200

    Add mouse movement to draw in canvas

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const ctx = canvas.getContext("2d")
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-ctx.beginPath()
-ctx.rect(50, 50, 100, 100)
-ctx.stroke()
+
+canvas.addEventListener("mousemove", (e) => {
+    ctx.beginPath()
+    ctx.rect(e.x, e.y, 10, 10)
+    ctx.stroke()
+    console.log(e)
+})
