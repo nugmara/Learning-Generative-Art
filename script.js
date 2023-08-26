58afd924b76b2bf22176512134b4abe192abf892
Dagmara Grabowska <dagmarag0@gmail.com>
2023-08-26 11:57:26 +0200

    Generate a nike Sky-Stars Effect

--- script.js
+++ script.js
@@ -8,21 +8,21 @@ const gradient = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
 gradient.addColorStop(0, "#363636");
 gradient.addColorStop(0.5, "#e7133f");
 gradient.addColorStop(1, "#ff83a5")
-ctx.fillStyle = gradient;
+ctx.fillStyle = "white";
 
 let atoms = [];
 
-canvas.addEventListener("mousemove", (e) => {
-    for (let i = 0; i < 20; i++) {
-        atoms.push(new Atom(e.x, e.y))     
-0    }
-})
+// canvas.addEventListener("mousemove", (e) => {
+//     for (let i = 0; i < 20; i++) {
+//         atoms.push(new Atom(e.x, e.y))     
+// 0    }
+// })
 
 class Atom {
     constructor(x, y) {
         this.x = x;
         this.y = y;
-        this.radius = Math.random() * 8 + 2;
+        this.radius = Math.random() * 2 + 2;
         this.speedX = Math.random() * 4 - 2;
         this.speedY = Math.random() * 4 - 2;
     }
@@ -41,8 +41,14 @@ class Atom {
     }
 }
 
+const generateAtoms = () => {
+    atoms.push(new Atom(Math.random() * canvas.width, Math.random() * canvas.height));
+    requestAnimationFrame(generateAtoms)
+}
+generateAtoms()
+
 const animate = () => {
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    // ctx.clearRect(0, 0, canvas.width, canvas.height)
     // update
 
     // draw
@@ -50,13 +56,15 @@ const animate = () => {
         atom.draw()
         atom.updateSpeed()
         atom.updateSize()
-
+        
         if (atom.radius < 0.3) {
             atoms.splice(index, 1);
         }
     })
     ctx.save();
-    ctx.fillStyle = "rgba(255, 255, 255, 0.2"
+    // ctx.fillStyle = "rgba(255, 255, 255, 0.2)"
+    ctx.fillStyle = "rgba(0, 0, 0, 0.2)"
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
     ctx.restore()
     requestAnimationFrame(animate)
 }
